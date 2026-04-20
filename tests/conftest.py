ab96b86692a1da8a1ad2a5821ceb4339308dbd96
jayen120 <jayadev786@rediffmail.com>
2026-04-20 20:14:07 +0530

    Day 7: Added environment based configuration and dynamic BASE_URL handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+import os
 import pytest
 from pages.login_page import LoginPage
 from utils.config import BASE_URL
 
+
+def pytest_configure():
+    print(f"\nRunning tests in ENV: {os.getenv('ENV', 'dev')}\n")
 
 @pytest.fixture
 def login_page(page):
--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,18 @@
-BASE_URL = "https://www.saucedemo.com/"
+import os
+
+ENV = os.getenv("ENV", "dev")
+
+CONFIG = {
+    "dev": {
+        "BASE_URL": "https://www.saucedemo.com/"
+    },
+    "staging": {
+        "BASE_URL": "https://www.saucedemo.com/"
+    }
+}
+
+BASE_URL = CONFIG[ENV]["BASE_URL"]
+
+
 USERNAME = "standard_user"
 PASSWORD = "secret_sauce"
