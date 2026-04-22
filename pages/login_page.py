09e72291e060d322ae6c11a382aace88201c2c0f
jayen120 <jayadev786@rediffmail.com>
2026-04-22 19:47:02 +0530

    Day 9: Implemented logging system with file and console output for better debugging

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,27 +1,35 @@
-from pages.base_page import BasePage
+from utils.logger import get_logger
 
-class LoginPage(BasePage):
+class LoginPage:
     def __init__(self, page):
-        super().__init__(page)
+        self.page = page
+        self.logger = get_logger(self.__class__.__name__)
 
-    # Locators
-    username_input = "#user-name"
-    password_input = "#password"
-    login_button = "#login-button"
-    error_message = '[data-test="error"]'
-    inventory_list = ".inventory_list"
+        # Locators
+        self.username_input = "#user-name"
+        self.password_input = "#password"
+        self.login_button = "#login-button"
+        self.error_message = '[data-test="error"]'
 
+    def open(self, url):
+        self.logger.info(f"Opening URL: {url}")
+        self.page.goto(url)
 
-    #Actions
+    # Actions
     def login(self, username, password):
-        self.fill(self.username_input, username)
-        self.fill(self.password_input, password)
-        self.click(self.login_button)
+        self.logger.info(f"Attempting login with user: {username}")
+
+        self.page.fill(self.username_input, username)
+        self.page.fill(self.password_input, password)
+        self.page.click(self.login_button)
 
     def get_error(self):
-        return self.get_text(self.error_message)
+        self.logger.info("Fetching error message")
+        self.logger.warning("Login failed, fetching error message")
+        return self.page.locator(self.error_message).inner_text()
     
     def is_inventory_page_visible(self):
-        self.page.wait_for_selector(self.inventory_list)
-        return self.page.locator(self.inventory_list).is_visible()
-        
+        self.logger.info("Checking if inventory page is visible")
+        # self.page.wait_for_selector(self.inventory_list)
+        return self.page.locator(".inventory_list").is_visible()
+
---
+++ utils/logger.py
@@ -0,0 +1,34 @@
+import logging
+import os
+
+def get_logger(name):
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, "test.log")
+
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+
+        # File handler
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.INFO)
+
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+
+        # Format
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
