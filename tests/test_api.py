d6d7aef46afadb2696709bfc97e482e939d72317
jayen120 <jayadev786@rediffmail.com>
2026-05-05 14:00:56 +0530

    Day 11: Added POST API test, enhanced GET validation, and response structure checks

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,3 +13,30 @@ def test_get_products():
 
     assert isinstance(data, list)
     assert len(data) > 0
+
+    first_product = data[0]
+
+    assert "title" in first_product
+    assert "price" in first_product
+    assert isinstance(first_product["price"], (int, float))
+
+@pytest.mark.api
+def test_create_product():
+    client = APIClient("https://fakestoreapi.com")
+
+    payload = {
+        "title" : "Test Product",
+        "price" : 100,
+        "description": "Test Description",
+        "image": "https://i.pravatar.cc",
+        "category": "electronics"
+    }
+
+    response = client.post("/products", payload)
+
+    assert response.status_code == 200 or response.status_code == 201
+
+    data = response.json()
+
+    assert data["title"] == payload["title"]
+    assert data["price"] == payload["price"]
